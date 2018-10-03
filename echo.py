53f969da05e9ce5d3c859c3b73b327a6dba2e3a6
borislitv <podstavnoe@gmail.com>
2018-10-03 16:23:57 +0300

    Update echo.py

--- echo.py
+++ echo.py
@@ -1,20 +1,30 @@
 #!/usr/bin/env python
 
+from cocaine.logger import Logger
 from cocaine.worker import Worker
 import msgpack
 
 DEFAULT_HEADERS = [('Content-type', 'text/plain')]
 
+log = Logger()
 
 def echo(request, response):
+    log.info("entire echo()")
     yield request.read()
+    log.info("do echo().request.read()")
     response.write(msgpack.packb((200, DEFAULT_HEADERS)))
+    log.info("do echo().response.write() with code and headers")
     response.write("echo")
+    log.info("do echo().response.write(echo), next echo().response.close()")
     response.close()
+    log.info("do echo().response.close() and exit now")
 
 def main():
+    log.info("entire main()")
     w = Worker()
+    log.info("do main().Worker()")
     w.run({"http": echo})
 
 if __name__ == "__main__":
+    log.info("start echo application worker. prior main()")
     main()
